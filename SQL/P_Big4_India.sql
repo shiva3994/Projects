236a204e983dd1d36cb4939311bc9baf58584bd6
Shiva Thakur <63214655+shiva3994@users.noreply.github.com>
2026-09-08 20:47:55 +0530

    Update P_Big4_India.sql

--- SQL/P_Big4_India.sql
+++ SQL/P_Big4_India.sql
@@ -327,11 +327,26 @@ ORDER BY Popularity DESC;
 
 
 -- Type B — TOP N
+
 -- 6. Find the top 5 highest-paid employees.
+
+SELECT TOP 5 *
+FROM Employees
+ORDER BY salary DESC;
+
 -- 7. Find the top 3 clients by ContractValue.
+
+
+
 -- 8. Find the top 5 projects by Budget.
+
+
 -- 9. Find the top 3 companies by Revenue in a given year.
+
+
 -- 10. Find the top 5 technologies by Popularity.
+
+
 
 -- Type C — Multi-column sorting
 -- 11. Sort Employees by Department ascending, then Salary descending within each department.
